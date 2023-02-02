076344a014cffd85319eb43277186676eaf19562
Prakash Rayka <120012353+PrakashRayka@users.noreply.github.com>
2023-02-02 21:07:13 +0530

    Add files via upload

---
+++ Codes/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        144 : '36rem',
+      }
+    },
+  },
+  plugins: [],
+}
